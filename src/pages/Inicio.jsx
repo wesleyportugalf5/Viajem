9386f1fca329caeee35f57ab9baa5a1570035a2d
Wesley Portugal <wesleyportugal250@gmail.com>
2026-02-24 04:49:48 -0300

    Estrutura inicial projeto

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import Menu from "./Menu";
+function Inicio() {
+    return (
+        <div>
+            <h1>Início</h1>
+            <Menu />
+        </div>
+    )
+}
+
+export default Inicio
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu() {
+    return (
+        <div>
+            <a href="/">Início</a>
+            <a href="/sobre">Sobre</a>
+            <a href="/contato">Contato</a>
+        </div>
+    )
+}
+
+export default Menu
