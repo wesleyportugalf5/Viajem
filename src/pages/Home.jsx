570ad4a94026651bad80b673af29bfef18e63551
Wesley Portugal <wesleyportugal250@gmail.com>
2026-02-25 23:20:57 -0300

    Categoria Brasil Página Home adicionada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,35 @@ import RioGrandedoSul from "../img/RioGrandedoSul.jpg";
 function Home() {
     return (
         <div className="home">
+
             <section className="homeDestaque">
                 <div className="destaqueContainer">
                     <img src={Campinas} alt="Imagem da cidade de Campinas" className="destaqueImg" />
                     <h1 className="destaqueTexto">Por que muitos de São Paulo - SP estão indo morar em Campinas?</h1>
                 </div>
             </section>
+
+            <section className="categoriasHome">
+
+                <h2 className="categoriasTitulo">Brasil</h2>
+                <section className="categoriasContainer">
+                    
+                    <div className="categoriasContainerTwo">
+                        <img src={Campinas} alt="" className="categoriasImg" />
+                        <p className="categoriasDescricao">Campinas: Princesa d’Oeste</p>
+                    </div>
+                    <div className="categoriasContainerTwo">
+                        <img src={RiodeJaneiro} alt="" className="categoriasImg" />
+                        <p className="categoriasDescricao">Rio de Janeiro: Cidade Maravilhosa</p>
+                    </div>
+                    <div className="categoriasContainerTwo">
+                        <img src={MinasGerais} alt="" className="categoriasImg" />
+                        <p className="categoriasDescricao">Minas Gerais: Terra do Pão de Queijo.</p>
+                    </div>
+                </section>
+
+            </section>
+
         </div>
     )
 }
