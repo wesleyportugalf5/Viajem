29f09dc07c2804d8506ae7a3d1ce77f81338b4e0
Wesley Portugal <wesleyportugal250@gmail.com>
2026-02-28 13:11:04 -0300

    Pagina China Pronta

--- src/pages/China.jsx
+++ src/pages/China.jsx
@@ -1,7 +1,27 @@
+import ChinaTwo from "../img/China02.jpg";
+import ChinaThree from "../img/China03.jpg";
+
 function China() {
     return (
         <div>
-            <h1>China</h1>
+            <h2 className="tituloPaginas">China: Grandes exportadores</h2>
+            <br />
+            <p>
+            A China une passado e modernidade. A Grande Muralha é um dos monumentos mais impressionantes do mundo. A Cidade Proibida revela séculos de história imperial.
+            </p>
+
+            <img src={ChinaThree} alt="" className="imagensPaginas" />
+            
+
+            <p>
+            Em Xi’an, o Exército de Terracota impressiona pela grandiosidade. Já Xangai simboliza o dinamismo econômico do país.
+            </p>
+
+            <img src={ChinaTwo} alt="" className="imagensPaginas" />
+
+            <p>
+            A culinária varia entre regiões, com destaque para o pato laqueado de Pequim e os pratos apimentados de Sichuan.
+            </p>
         </div>
     )
 }
