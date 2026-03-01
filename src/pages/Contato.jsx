80fa17463a7f3b102fc093b1d25857e8999c98e4
Wesley Portugal <wesleyportugal250@gmail.com>
2026-03-01 17:41:48 -0300

    Página sobre e contato prontas

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,7 +1,30 @@
+import JapaoThree from "../img/Japao03.jpg";
+
 function Contato() {
     return (
-        <div>
-            <h1>Contato</h1>
+        <div className="contato">
+            <h2 className="tituloPaginas" id="tituloContato">Entre em Contato</h2>
+
+            <p className="subtituloContato">Para uma experiência incrível comigo. Valores a partir de R$1500</p>
+
+            <form className="formularioContato" onsubmit="enviarWhats(event)">
+
+            <div className="grupoForm">
+                <input placeholder="Nome" className="campoForm" />
+            </div>
+            <div className="grupoForm">
+                <textarea className="campoForm" placeholder="Digite sua Mensagem" rows="6"></textarea>
+            </div>
+            <button type="submit" className="botaoForm">Enviar WhatsApp</button>
+            </form>
+
+            <br />
+
+            <p>No máximo, em 2 dias retorno o contato. Será um prazer, vivermos essa experiência juntos.</p>
+
+            <img src={JapaoThree} alt="" className="imagensPaginas" />
+
+            <p>Como estou passando um tempo no Japão, você pode passar um dia inesquecível comigo, conhecendo esse país incrível por metade do preço.</p>
         </div>
     )
 }
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,7 +1,21 @@
+import RiodeJaneiroTwo from "../img/RiodeJaneiro02.jpg";
+import AlemanhaThree from "../img/Alemanha03.jpg";
+
 function Sobre() {
     return (
         <div>
-            <h1>Sobre</h1>
+            <h2 className="tituloPaginas">Sobre</h2>
+            <br />
+
+            <p>O Viagem foi criado com o objetivo de ser um guia turístico, com gastronomia locais, restaurantes que precisam ser visitados, lugares imperdíveis, história da região, cultura.</p>
+            
+            <img src={RiodeJaneiroTwo} alt="" className="imagensPaginas" /> 
+
+            <p>Nosso objetivo é facilitar a vida das pessoais, fornecendo um site, onde estão reunidos as coisas mais relevante a cerca de cada lugar, seja cuidados a serem tomados, um guia total.</p>
+
+            <img src={AlemanhaThree} alt="" className="imagensPaginas" />
+
+            <p>Explore o site, e planeje já sua próxima viagem!!</p>
         </div>
     )
 }
